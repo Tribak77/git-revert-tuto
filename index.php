745cbc2f867cbd20555019ca67c11049f5e41212
Tribak77 <atribak34@gmail.com>
2024-10-09 13:20:22 +0100

    use git revet to come back to a previous commit

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 
+<<<<<<< HEAD
 echo "the first action";
 echo "the second action";
 echo "the third action";
+=======
+echo "the first action";
+>>>>>>> parent of 0e76d58 (the second action)
 ?>

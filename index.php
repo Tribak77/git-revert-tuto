49f575ba165b6efbd24134281118e2b7c588f5fa
Tribak77 <atribak34@gmail.com>
2024-10-09 13:32:48 +0100

    make some changes

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
 
-<<<<<<< HEAD
 echo "the first action";
 echo "the second action";
 echo "the third action";
-=======
-echo "the first action";
->>>>>>> parent of 0e76d58 (the second action)
 ?>
